d5b037f727c8f1d2726dc28381f6c060a5db1996
EnzoCordovana <enzo.cordovana@etu.univ-amu.fr>
2025-06-10 09:02:38 +0200

    feat: button qui permet de fermer la scene

--- src/main/java/fr/amu/iut/bomberman/components/ExitButton.java
+++ src/main/java/fr/amu/iut/bomberman/components/ExitButton.java
@@ -1,4 +1,30 @@
 package fr.amu.iut.bomberman.components;
 
-public class ExitButton {
+import javafx.stage.Stage;
+
+/**
+ * Bouton personnalisé pour quitter le Stage.
+ */
+public class ExitButton extends CustomButton {
+
+    private final Stage stage;
+
+    /**
+     * Constructeur de ExitButton.
+     *
+     * @param stage Le Stage à fermer.
+     */
+    public ExitButton(Stage stage) {
+        super("Quitter");
+        this.stage = stage;
+        setButtonAction();
+    }
+
+    /**
+     * Définit l'action du bouton pour fermer le Stage.
+     */
+    @Override
+    public void setButtonAction() {
+        this.setOnAction(event -> stage.close());
+    }
 }
